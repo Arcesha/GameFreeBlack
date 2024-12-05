1e4a991ab26e33009066e35eeceaaba6a7bca743
Arcesha <157727872+Arcesha@users.noreply.github.com>
2024-12-05 20:33:03 +0300

    999

--- Game_FreeBlackConsole/GameNegru/GameNegru/GameNegru.cpp
+++ Game_FreeBlackConsole/GameNegru/GameNegru/GameNegru.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include "Game.h"
 
-int main() {
-	Game game;
-	Game run();
-	return 0;
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        return -1;
+    }
+    SDL_Quit();
+    return 0;
 }
